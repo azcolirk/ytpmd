e2ea384ad1403509a57757802d916bb43d3a6924
Александр Звозников <a.zvoznikov@ispsystem.com>
2018-07-21 18:24:04 +0800

    example react app

--- examples/react/ClientApp/src/index.js
+++ examples/react/ClientApp/src/index.js
@@ -1,5 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'uikit/dist/js/uikit.js';
+import 'uikit/dist/js/uikit-icons.js';
+import 'uikit/dist/css/uikit.css';
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
