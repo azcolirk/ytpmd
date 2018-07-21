b66c3dea681cc8811d90dbf16a9fa5bbdc803478
Александр Звозников <a.zvoznikov@ispsystem.com>
2018-07-21 18:24:04 +0800

    example react app

--- examples/react/ClientApp/src/components/Home.js
+++ examples/react/ClientApp/src/components/Home.js
@@ -20,6 +20,10 @@ export class Home extends Component {
           <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
         </ul>
         <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+          <h3 class="uk-card-title">Default</h3>
+          <p>Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
       </div>
     );
   }
